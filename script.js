4acccf75625d9fee34ab18a198b596391471fa32
bwondessen <bwondessen23@gmail.com>
2024-12-06 14:07:02 -0600

    Create displayAuthors function

--- script.js
+++ script.js
@@ -14,3 +14,8 @@ fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
         console.error(`There was an error: ${err}`);
     });
 
+const displayAuthors = (authors) => {
+    authors.forEach(({ author, image, url, bio }, index) => {
+
+    });
+};
